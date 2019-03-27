f34f3c3216385ca99016967c88e27ceef9f96a38
olympia <owojcik7@gmail.com>
2019-03-27 17:24:19 -0400

    Add ability to get and get by id

--- cohorts/cohorts-router.js
+++ cohorts/cohorts-router.js
@@ -5,6 +5,7 @@ const knex = require("knex");
 const knexConfig = require("../knexfile.js");
 const db = knex(knexConfig.development);
 
+//- `[GET] /api/cohorts`
 router.get("/", (req, res) => {
   console.log(req.body);
   db("cohorts")
@@ -15,3 +16,13 @@ router.get("/", (req, res) => {
 });
 
 module.exports = router;
+
+//- `[GET] /api/cohorts/:id`
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+  db("cohorts")
+    .where({ id: req.params.id })
+    .first()
+    .then(cohort => res.status(200).json(cohort))
+    .catch(err => res.status(500).json(error));
+});
